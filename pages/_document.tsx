185279594128a8fee531437d6ec500d6e9852f06
Sangmoo <first_1st@naver.com>
2021-03-24 22:00:18 +0900

    Set Font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document from "next/document"
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
         const sheet = new ServerStyleSheet();
         const originalRenderPage = ctx.renderPage;
 
@@ -27,4 +27,21 @@ export default class MyDocument extends Document {
             sheet.seal();
         } 
     }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR:400,700&display=swap&subset=korean" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
+
+export default MyDocument;
--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,11 +1,19 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle, css } from "styled-components"
 import reset from "styled-reset"
 
-const GlobalStyle = createGlobalStyle`
-    ${reset}
+const globalStyle = css`
+    ${reset};
+
     * {
         box-sizing: border-box;
     }
+    body {
+        font-family: Noto Sans, Noto Sans KR;
+    }
+`;
+
+const GlobalStyle = createGlobalStyle`
+    ${globalStyle}
 `;
 
 export default GlobalStyle;
